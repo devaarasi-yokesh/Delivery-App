c136432029bd82b4f6d839d0a8ecd61af322c7d4
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2024-04-15 16:45:26 +0530

    Add files via upload

---
+++ Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+    </div>
+  )
+}
+
+export default ExploreMenu
